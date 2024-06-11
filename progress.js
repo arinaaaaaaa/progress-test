1cc6581d38ebd4bc65b3e95be4f4c0bd120c2c4e
akrumm <akrumm@k-tech.ru>
2024-06-11 21:01:53 +0300

    add hide and animate functions

--- progress.js
+++ progress.js
@@ -1,7 +1,11 @@
+const progressBar = document.getElementById('progressCircle')
+const valueInput = document.getElementById('value');
+const animateInput = document.getElementById('animation');
+const hideInput = document.getElementById('hide');
+
 // Функция валидации значения прогресса (от 0 до 100)
 function validateProgressValue() {
-    let inputField = document.getElementById('progressValue');
-    let inputValue = inputField.value || '0';
+    let inputValue = valueInput.value || '0';
 
     // Удаление нулей в начале значения
     if (inputValue.length > 1)
@@ -9,23 +13,33 @@ function validateProgressValue() {
 
     // Удаление недопустимых символов (букв, символов, значений, выходящих за допустимый диапозон)
     if (!isFinite(inputValue) || inputValue > 100)
-        inputValue = inputValue.length === 0 ? 0 : inputValue.slice(0, -1);
+        inputValue = inputValue.slice(0, -1);
 
-    inputField.value = inputValue;
+    if (inputValue.length === 0) inputValue = 0;
+
+    animateInput.disabled = inputValue === "0";
+    valueInput.value = inputValue;
 }
 
 //Функция изменения значения прогресса
 function updateProgressValue() {
-    const circle = document.getElementById('progressCircle');
     validateProgressValue();
-    const percentage = document.getElementById('progressValue').value;
+    const percentage = valueInput.value;
 
     // Закрашивание области прогресса
-    circle.style.background = `conic-gradient(#3e4fff ${percentage}%, #e0e0e0 ${percentage}%)`;
+    progressBar.style.background = `conic-gradient(#3e4fff ${percentage}%, #e0e0e0 0%)`;
 }
 
-document.getElementById('progressValue').addEventListener('input', function(e) {
-    updateProgressValue();
+valueInput.addEventListener('input', () => updateProgressValue());
+updateProgressValue(valueInput.value);
+
+// Изменение анимации при взаимодействии со свичем Animate
+animateInput.addEventListener('change', () => {
+    progressBar.style.animation = animateInput.checked ? "rotate 3s linear infinite" : "";
 });
 
-updateProgressValue(document.getElementById('progressValue').value);
+// Изменение видимости при взаимодействии со свичем Hide
+hideInput.addEventListener('change', () => {
+    progressBar.style.visibility = hideInput.checked ? "hidden" : "";
+    animateInput.disabled = hideInput.checked;
+});
