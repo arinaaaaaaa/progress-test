611678de20bd28172144a8926838656096ca437d
akrumm <akrumm@k-tech.ru>
2024-06-12 20:02:31 +0300

    add styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 // Компонент поля ввода значения прогресса
-const valueInput = document.getElementById('vlue');
+const valueInput = document.getElementById('value');
 // Компонент переключателя состояния Animate
-const animateInput = document.getElementById('animaton');
+const animateInput = document.getElementById('animation');
 // Компонент переключателя состояния Hide
-const hideInput = document.getElementById('hie');
+const hideInput = document.getElementById('hide');
 
 //Функция настройки доступности свича Animate
 const setAnimateSwitcherState = (value, hide) => {
